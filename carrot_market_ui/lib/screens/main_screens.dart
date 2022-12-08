cb831d71a5927b688d6265cc2fdff19e90e7d61c
alee <alee@aleeui-MacBookPro.local>
2022-12-08 15:40:25 +0900

    feat: indexed screen

--- carrot_market_ui/lib/screens/main_screens.dart
+++ carrot_market_ui/lib/screens/main_screens.dart
@@ -1,4 +1,7 @@
+import 'package:carrot_market_ui/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreens extends StatefulWidget {
   const MainScreens({Key? key}) : super(key: key);
@@ -8,11 +11,54 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('MainScreens'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Container(
+            color: Colors.orange[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 1',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.redAccent[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 2',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: theme().primaryColor,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            this._selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.home), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble), label: '채팅'),
+        ],
       ),
     );
   }
