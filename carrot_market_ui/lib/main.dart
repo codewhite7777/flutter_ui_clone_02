99a38f2eba8099d8a3928111892a49b45b50b6b1
alee <alee@aleeui-MacBookPro.local>
2022-12-08 15:48:44 +0900

    temp screens

---
+++ carrot_market_ui/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:carrot_market_ui/screens/main_screens.dart';
+import 'package:carrot_market_ui/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(CarrotMarketUI());
+
+class CarrotMarketUI extends StatelessWidget {
+  const CarrotMarketUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      title: 'carrot_market_ui',
+      debugShowCheckedModeBanner: false,
+      home: MainScreens(),
+    );
+  }
+}
